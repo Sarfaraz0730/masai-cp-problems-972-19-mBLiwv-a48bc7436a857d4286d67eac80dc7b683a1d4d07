5cee7cc6eddd1bbc65b306e898af71bf6871c1d3
=Sarfaraz0730 <72138410+Sarfaraz0730@users.noreply.github.com>
2023-02-27 10:48:43 +0530

    Country card part completed

---
+++ src/Component/Countries.jsx
@@ -0,0 +1,82 @@
+import LoadingIndicator from "./LoadingIndicator";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function Countries() {
+  const [data, setData] = useState();
+  // return <LoadingIndicator />;
+
+  //  const EditDescription = async (form: any) => {
+  //    try {
+  //      const result = await axios.patch(
+  //        "http://localhost:80/client/updatePostDescription",
+  //        {},
+  //        {
+  //          params: form,
+  //          headers: {
+  //            Authorization: `Bearer ${token}`,
+  //          },
+  //        }
+  //      );
+  //      setPostEdit(result.data);
+  //      showSnackbar("Your Post has been edited");
+  //    } catch (err) {
+  //      console.log(err);
+  //    }
+  //  };
+  const countriesData = async () => {
+    try {
+      const result = await axios.get(
+        "https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-countries"
+      );
+
+      setData(result.data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    countriesData();
+  }, []);
+  console.log("data", data);
+
+  return (
+    <div>
+      <h1 data-testid="countries-header">Countries List</h1>
+      <div data-testid="countries-container">
+        {/* Countries Card */}
+        {/* {data ? (
+          data.map((e, i) => {
+            return <div key={i}>Countries : {data}</div>;
+          })
+        ) : (
+          <LoadingIndicator />
+        )} */}
+        {data ? (
+          data.map((e, i) => {
+            return (
+              <div
+                style={{
+                  border: "3px solid black",
+                  margin: "10px",
+                  padding: "10px",
+                  borderRadius: "10px",
+                  fontSize: "25px",
+                  fontWeight: 400,
+                }}
+              >
+                <div key={i}> Country : {e.country}</div>
+                <div key={i}> population : {e.population}</div>
+              </div>
+            );
+          })
+        ) : (
+          <LoadingIndicator />
+        )}
+      </div>
+      <div>{/* Pagination */}</div>
+    </div>
+  );
+}
+
+export default Countries;
